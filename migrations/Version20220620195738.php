01d2b3f90f3b4527e7ce2a7206bef718aefebe49
sidlerbiz <sidlerbiz@gmail.com>
2022-06-22 02:57:35 +0300

    Init

---
+++ migrations/Version20220620195738.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20220620195738 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE company (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(100) NOT NULL, financial_status VARCHAR(1) NOT NULL, market_category VARCHAR(1) NOT NULL, security_name VARCHAR(255) NOT NULL, round_lot_size INT NOT NULL, symbol VARCHAR(5) NOT NULL, test_issue VARCHAR(1) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE company');
+    }
+}
